e219273ecc1788fff55f3145c52678f488a693c3
Mahmoud Elkabbany <mahmoudelkabbany292@gmail.com>
2022-08-11 08:27:31 +0200

    Add functionality for clearing all screen

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const screen = document.querySelector(".screen");
 const insertButtons = document.querySelectorAll(".insert");
+const clearAllButton = document.getElementById("clear-all");
 screen.textContent = "";
+
 for (let i = 0; i < insertButtons.length; i++) {
   insertButtons[i].addEventListener("click", () => {
     screen.textContent += insertButtons[i].textContent;
   });
 }
+
+clearAllButton.addEventListener("click", () => {
+  screen.textContent = "";
+});
