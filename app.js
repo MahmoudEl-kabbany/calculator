6925fb2dcd1872f616244a79825c753af56cb3d4
Mahmoud Elkabbany <mahmoudelkabbany292@gmail.com>
2022-08-11 09:19:17 +0200

    Add functionality for all operators

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
 const screen = document.querySelector(".screen");
 const insertButtons = document.querySelectorAll(".insert");
 const clearAllButton = document.getElementById("clear-all");
