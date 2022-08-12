4852dcc5a17c0de57394fdda9bd7c6fcb93d1490
Mahmoud Elkabbany <mahmoudelkabbany292@gmail.com>
2022-08-12 09:35:58 +0200

    Allow only one operator between numbers

--- app.js
+++ app.js
@@ -19,26 +19,47 @@ const numberButtons = document.querySelectorAll(".insert-number");
 const operatorButtons = document.querySelectorAll(".insert-operator");
 const clearAllButton = document.getElementById("clear-all");
 const clearOneButton = document.getElementById("clear-one");
-screen.textContent = "";
+screen.textContent = "0";
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const operators = ["÷", "+", "−", "x"];
 
 for (let i = 0; i < numberButtons.length; i++) {
   numberButtons[i].addEventListener("click", () => {
-    screen.textContent += numberButtons[i].textContent;
+    if (screen.textContent == "0") {
+      screen.textContent = numberButtons[i].textContent;
+    } else {
+      screen.textContent = screen.textContent + numberButtons[i].textContent;
+    }
+    for (let i = 0; i < operatorButtons.length; i++) {
+      operatorButtons[i].removeAttribute("disabled", "");
+    }
   });
 }
 
 for (let i = 0; i < operatorButtons.length; i++) {
   operatorButtons[i].addEventListener("click", () => {
     screen.textContent += operatorButtons[i].textContent;
+    for (let i = 0; i < operatorButtons.length; i++) {
+      operatorButtons[i].setAttribute("disabled", "");
+    }
   });
 }
 
 clearAllButton.addEventListener("click", () => {
-  screen.textContent = "";
+  screen.textContent = "0";
+  for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].removeAttribute("disabled", "");
+  }
 });
 
 clearOneButton.addEventListener("click", () => {
   let screenText = screen.textContent;
+  if (operators.includes(screenText[screenText.length - 1])) {
+    for (let i = 0; i < operatorButtons.length; i++) {
+      operatorButtons[i].removeAttribute("disabled", "");
+    }
+  }
   let newScreenText = screenText.slice(0, screenText.length - 1);
   screen.textContent = newScreenText;
 });
